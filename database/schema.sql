e40f0303265f554c65471b3451963fbb878cd313
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-26 17:00:15 +0200

    Apply group changes

--- database/schema.sql
+++ database/schema.sql
@@ -9,15 +9,41 @@ CREATE DATABASE catalog_of_my_things
 \c catalog_of_my_things
 
 -- Create tables:
+
 CREATE TABLE author (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
-   PRIMARY KEY(id)
+);
+
+CREATE TABLE genre (
+  id SERIAL PRIMARY KEY,
+  name NARCHAR(50)
+  );
+
+CREATE TABLE label (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(250),
+  color VARCHAR(250)
+);
+
+CREATE TABLE item (
+id SERIAL PRIMARY KEY,
+publish_date DATE NOT NULL,
+archived BOOLEAN NOT NULL,
+label_id INT REFERENCES label(id),
+author_id INT REFERENCES author(id),
+genre_id INT REFERENCES genre(id),
 );
 
 CREATE TABLE game (
-  id INT PRIMARY KEY REFERENCES item(id),
   multiplayer VARCHAR(30) NOT NULL,
   last_played_at DATE NOT NULL
+  item_id INT references item(id)
+);
+
+CREATE TABLE book (
+item_id INT REFERENCES item(id),
+publisher VARCHAR(250) NOT NULL,
+cover_state VARCHAR(250) NOT NULL
 );
